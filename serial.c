0a3dcfcc19ca0d96c656fc19da815232857568ce
BALASURIYANV <electronengineer19@gmail.com>
2025-07-19 18:25:57 +0530

    Add files via upload

---
+++ serial.c
@@ -0,0 +1,24 @@
+/*
+ * File:   serial.c
+ * Author: balas
+ *
+ * Created on 6 June, 2025, 7:55 PM
+ */
+
+#pragma config FOSC = HS        
+#pragma config WDTE = OFF       
+#pragma config PWRTE = OFF      
+#pragma config BOREN = ON       
+#pragma config LVP = OFF         
+#pragma config CPD = OFF        
+#pragma config WRT = OFF        
+#pragma config CP = OFF
+#include<pic.h>
+#include <xc.h>
+
+
+
+
+void main(void) {
+    return;
+}
